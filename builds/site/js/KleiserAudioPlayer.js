d0246aefd9fcaa655b729799278d994c975da83c
Cory Kleiser <kleisecj@mail.uc.edu>
2017-01-27 23:46:16 -0500

    cleaned up code

--- builds/site/js/KleiserAudioPlayer.js
+++ builds/site/js/KleiserAudioPlayer.js
@@ -1,157 +1,129 @@
-'use strict';
-//vars
-const audioPlayers = Array.from(document.getElementsByTagName('audio'));
+(function(){
+    'use strict';
+    //vars
+    const audioPlayers = Array.from(document.getElementsByTagName('audio'));
 
-console.log(audioPlayers);
+    console.log(audioPlayers);
 
+    const insertAfter = (newNode, referenceNode) => referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 
-//functions
-const insertAfter = (newNode, referenceNode) => referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-
-const ff = track => {
-    if (track.playbackRate < 4) {
-        track.playbackRate += 1;
-        console.log(track.playbackRate);
-    }
-    else {
-        track.playbackRate = 1;
-        console.log(track.playbackRate);
-    }
-};
-
-
-
-let i = 0;
-
-audioPlayers.forEach(function (audio) {
-
-    //container
-    const audioContainer = document.createElement('div');
-    audioContainer.setAttribute('class', 'audio');
-
-    //set innerHTML
-    audioContainer.innerHTML = `
-        <div class="ap-scrubberContainer">
-            <div class="ap-scrubberBar">
-                <div style="width:0%" class="ap-scrubber">&nbsp;</div>
-            </div>
-        </div>
-        <div class="ap-controls">
-            <div class="ap-navigation">
-                <button class="ap-back10">&laquo;10</button>
-                <button class="ap-togglePlay"></button>
-                <button class="ap-skip10">10&raquo;</button>
-            </div>
-            <div class="ap-volume">
-                <button class="ap-volbtn">sp</button>
-                <section class="ap-volslider">
-                    <h1>Volume Slider Here</h1>
-                </section>
-            </div>
-        </div>        
-`;
-
-    //add to document
-    insertAfter(audioContainer, audio);
-
-
-
-    //set up button
-    const playPause = document.getElementsByClassName('ap-togglePlay')[i];
-    playPause.innerHTML = '&#9654;';
-    const togglePlayPause = Rx.Observable.fromEvent(playPause, 'click');
-    /**
-     * toggle play/pause track
-     */
-    togglePlayPause.forEach((e) => {
-        if (audio.paused) {
-            audio.play();
-            setTimeout(()=>console.log(`${audio.currentTime/audio.duration*100}%`), 1000);
-            playPause.innerHTML = '&#10073;&#10073;';
+    const ff = track => {
+        if (track.playbackRate < 4) {
+            track.playbackRate += 1;
+            console.log(track.playbackRate);
         }
         else {
-            audio.pause();
-            playPause.innerHTML = '&#9654;';
-        }
-    });
-
-    //set up nav controls
-
-    const back10 = document.getElementsByClassName('ap-back10')[i];
-    const back10sec = Rx.Observable.fromEvent(back10, 'click');
-    /**
-     * go back 10 seconds
-     */
-    back10sec.forEach((e) => audio.currentTime-=10);
-
-    const skip10 = document.getElementsByClassName('ap-skip10')[i];
-    /**
-     * skip forward 10 seconds
-     */
-    const skip10sec = Rx.Observable.fromEvent(skip10, 'click');
-    skip10sec.forEach((e) => audio.currentTime+=10);
-
-    //set up scrubber
-    const scrubberBox = document.getElementsByClassName('ap-scrubberContainer')[i];
-    const scrubber = document.getElementsByClassName('ap-scrubberBar')[i];
-    const currentPosition = document.getElementsByClassName('ap-scrubber')[i];
-
-    const scrubberClicks = Rx.Observable.fromEvent(scrubberBox, 'click');
-    /**
-     * skip to location in track based on scrubber clicks
-     */
-    scrubberClicks.forEach((e) => {
-        console.log(e);
-        const rect = scrubberBox.getBoundingClientRect();
-        const positionRequested = (e.clientX - rect.left) / rect.width;
-        audio.currentTime = audio.duration * positionRequested;
-    });
-
-    /**
-     * Animate Scrubber to time updates
-     */
-    audio.ontimeupdate = () => {
-        audio.percentPlayed = audio.currentTime/audio.duration*100;
-        currentPosition.setAttribute('style', `width: ${audio.percentPlayed}%`);
-        if (audio.percentPlayed === 100) {
-            button.innerHTML ='&#9654;'
+            track.playbackRate = 1;
+            console.log(track.playbackRate);
         }
     };
 
-    //todo:: find proper icons for volume button
-    const volume = document.getElementsByClassName('ap-volume')[i];
-    const volumeBtn = document.getElementsByClassName('ap-volbtn')[i];
-    const volumeSlider = document.getElementsByClassName('ap-volslider')[i];
-    const volumeClick = Rx.Observable.fromEvent(volumeBtn, 'click');
-    const volumeEnter = Rx.Observable.fromEvent(volume, 'mouseover');
-    const volumeExit = Rx.Observable.fromEvent(volume, 'mouseleave');
-    const currentVolume = audio.volume;
-    /**
-     * toggle mute on click
-     */
-    volumeClick.forEach(function (e) {
-        if(audio.volume > 0){
-            audio.volume = 0;
-        } else {
-            audio.volume = currentVolume;
-        }
-    });
-    /**
-     * volume controls to appear on mouse over
-     */
-    volumeEnter.forEach(function (e) {
-        console.log('working');
-        volumeSlider.style.display = 'inline';
-    });
-    /**
-     * volume controls to appear on mouse exit
-     */
-    volumeExit.forEach(function (e) {
-        //todo change on mouse out to preserve display
-        volumeSlider.style.display = 'none';
-    });
-    //todo:: enable dragable volume control
 
-    i++;
-    console.log(i);
-});
+
+    let i = 0;
+
+    audioPlayers.forEach(function (audio) {
+
+        //container
+        const audioContainer = document.createElement('div');
+        audioContainer.setAttribute('class', 'audio');
+
+        //set innerHTML
+        audioContainer.innerHTML = `
+            <div class="ap-scrubberContainer">
+                <div class="ap-scrubberBar">
+                    <div style="width:0%" class="ap-scrubber">&nbsp;</div>
+                </div>
+            </div>
+            <div class="ap-controls">
+                <div class="ap-navigation">
+                    <button class="ap-back10">&laquo;10</button>
+                    <button class="ap-togglePlay"></button>
+                    <button class="ap-skip10">10&raquo;</button>
+                </div>
+            </div>        
+    `;
+
+        //add to document
+        insertAfter(audioContainer, audio);
+
+
+
+        //set up button
+        const playPause = document.getElementsByClassName('ap-togglePlay')[i];
+        playPause.innerHTML = '&#9658;';
+        const togglePlayPause = Rx.Observable.fromEvent(playPause, 'click');
+        /**
+         * toggle play/pause track
+         */
+        togglePlayPause.forEach((e) => {
+            if (audio.paused) {
+                audio.play();
+                setTimeout(()=>console.log(`${audio.currentTime/audio.duration*100}%`), 1000);
+                playPause.innerHTML = '&#10073;&#10073;';
+            }
+            else {
+                audio.pause();
+                playPause.innerHTML = '&#9658;';
+            }
+        });
+
+        //set up nav controls
+
+        const back10 = document.getElementsByClassName('ap-back10')[i];
+        const back10sec = Rx.Observable.fromEvent(back10, 'click');
+        /**
+         * go back 10 seconds
+         */
+        back10sec.forEach((e) => audio.currentTime-=10);
+        const skip10 = document.getElementsByClassName('ap-skip10')[i];
+        /**
+         * skip forward 10 seconds
+         */
+        // const skip10sec = Rx.Observable.fromEvent(skip10, 'click');
+        // skip10sec.forEach((e) => audio.currentTime+=10);
+        /**
+         * fast forward
+         */
+        const fastForward = Rx.Observable.fromEvent(skip10, 'click');
+        fastForward.forEach((e) => ff(audio));
+
+        //set up scrubber
+        const scrubberBox = document.getElementsByClassName('ap-scrubberContainer')[i];
+        const scrubber = document.getElementsByClassName('ap-scrubberBar')[i];
+        const currentPosition = document.getElementsByClassName('ap-scrubber')[i];
+
+        const scrubberClicks = Rx.Observable.fromEvent(scrubberBox, 'click');
+
+        /**
+         * Animate buffer
+         */
+        audio.progress = () => {
+            console.log(audio.buffer);
+        }
+
+        /**
+         * skip to location in track based on scrubber clicks
+         */
+        scrubberClicks.forEach((e) => {
+            console.log(e);
+            const rect = scrubberBox.getBoundingClientRect();
+            const positionRequested = (e.clientX - rect.left) / rect.width;
+            audio.currentTime = audio.duration * positionRequested;
+        });
+
+        /**
+         * Animate Scrubber to time updates
+         */
+        audio.ontimeupdate = () => {
+            audio.percentPlayed = audio.currentTime/audio.duration*100;
+            currentPosition.setAttribute('style', `width: ${audio.percentPlayed}%`);
+            if (audio.percentPlayed === 100) {
+                playPause.innerHTML ='&#9658;'
+            }
+        };
+
+        i++;
+        console.log(i);
+    });
+}());
---
+++ builds/site/js/es5/KleiserAudioPlayer.js
@@ -0,0 +1,111 @@
+(function () {
+    'use strict';
+    //vars
+
+    var audioPlayers = Array.from(document.getElementsByTagName('audio'));
+
+    console.log(audioPlayers);
+
+    var insertAfter = function insertAfter(newNode, referenceNode) {
+        return referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+    };
+
+    var ff = function ff(track) {
+        if (track.playbackRate < 4) {
+            track.playbackRate += 1;
+            console.log(track.playbackRate);
+        } else {
+            track.playbackRate = 1;
+            console.log(track.playbackRate);
+        }
+    };
+
+    var i = 0;
+
+    audioPlayers.forEach(function (audio) {
+
+        //container
+        var audioContainer = document.createElement('div');
+        audioContainer.setAttribute('class', 'audio');
+
+        //set innerHTML
+        audioContainer.innerHTML = '\n            <div class="ap-scrubberContainer">\n                <div class="ap-scrubberBar">\n                    <div style="width:0%" class="ap-scrubber">&nbsp;</div>\n                </div>\n            </div>\n            <div class="ap-controls">\n                <div class="ap-navigation">\n                    <button class="ap-back10">&laquo;10</button>\n                    <button class="ap-togglePlay"></button>\n                    <button class="ap-skip10">10&raquo;</button>\n                </div>\n            </div>        \n    ';
+
+        //add to document
+        insertAfter(audioContainer, audio);
+
+        //set up button
+        var playPause = document.getElementsByClassName('ap-togglePlay')[i];
+        playPause.innerHTML = '&#9658;';
+        var togglePlayPause = Rx.Observable.fromEvent(playPause, 'click');
+        /**
+         * toggle play/pause track
+         */
+        togglePlayPause.forEach(function (e) {
+            if (audio.paused) {
+                audio.play();
+                setTimeout(function () {
+                    return console.log(audio.currentTime / audio.duration * 100 + '%');
+                }, 1000);
+                playPause.innerHTML = '&#10073;&#10073;';
+            } else {
+                audio.pause();
+                playPause.innerHTML = '&#9658;';
+            }
+        });
+
+        //set up nav controls
+
+        var back10 = document.getElementsByClassName('ap-back10')[i];
+        var back10sec = Rx.Observable.fromEvent(back10, 'click');
+        /**
+         * go back 10 seconds
+         */
+        back10sec.forEach(function (e) {
+            return audio.currentTime -= 10;
+        });
+        var skip10 = document.getElementsByClassName('ap-skip10')[i];
+        /**
+         * skip forward 10 seconds
+         */
+        // const skip10sec = Rx.Observable.fromEvent(skip10, 'click');
+        // skip10sec.forEach((e) => audio.currentTime+=10);
+        /**
+         * fast forward
+         */
+        var fastForward = Rx.Observable.fromEvent(skip10, 'click');
+        fastForward.forEach(function (e) {
+            return ff(audio);
+        });
+
+        //set up scrubber
+        var scrubberBox = document.getElementsByClassName('ap-scrubberContainer')[i];
+        var scrubber = document.getElementsByClassName('ap-scrubberBar')[i];
+        var currentPosition = document.getElementsByClassName('ap-scrubber')[i];
+
+        var scrubberClicks = Rx.Observable.fromEvent(scrubberBox, 'click');
+        /**
+         * skip to location in track based on scrubber clicks
+         */
+        scrubberClicks.forEach(function (e) {
+            console.log(e);
+            var rect = scrubberBox.getBoundingClientRect();
+            var positionRequested = (e.clientX - rect.left) / rect.width;
+            audio.currentTime = audio.duration * positionRequested;
+        });
+
+        /**
+         * Animate Scrubber to time updates
+         */
+        audio.ontimeupdate = function () {
+            audio.percentPlayed = audio.currentTime / audio.duration * 100;
+            currentPosition.setAttribute('style', 'width: ' + audio.percentPlayed + '%');
+            if (audio.percentPlayed === 100) {
+                playPause.innerHTML = '&#9658;';
+            }
+        };
+
+        i++;
+        console.log(i);
+    });
+})();
